212c1e413a543d3c044a38e5ea773701db61cf58
Damn <G1n7oki@163.com>
2023-02-16 18:52:07 +0800

    feat: 新增SvgIcon组件

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    v-if="isExternal"
+    :style="styleExternalIcon"
+    class="svg-external-icon svg-icon"
+    :class="className"
+  />
+  <svg v-else class="svg-icon" :class="className" aria-hidden="true">
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+
+<script setup>
+import { isExternal as external } from '@/utils/validate'
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+  // 图标的名称
+  icon: {
+    type: String,
+    required: true
+  },
+  // 图标的类名
+  className: {
+    type: String,
+    default: ''
+  }
+})
+// 判断当前图表是否为外部图标
+const isExternal = computed(() => external(props.icon))
+// 外部的图标样式
+const styleExternalIcon = computed(() => ({
+  mask: `url(${props.icon}) no-repeat 50% 50%`,
+  '-webkit-mask': `url(${props.icon}) no-repeat 50% 50%`
+}))
+// 内部图标
+const iconName = computed(() => `#icon-${props.icon}`)
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+.svg-external-icon {
+  display: inline-block;
+  background-color: currentColor;
+  mask-size: cover !important;
+}
+</style>
